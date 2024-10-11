7611f81e630d0110cc21505f32f1f9cea4b23b5e
Gil Sinclair-Julio <gil@golightyear.com>
2024-10-11 12:00:36 +0100

    Add core volt classes

---
+++ lib/src/hooks/use_query.dart
@@ -0,0 +1,9 @@
+import 'package:volt/src/query.dart';
+
+T? useQuery<T>(
+  VoltQuery<T> query, {
+  Duration? staleTime,
+  bool enabled = true,
+}) {
+  return null;
+}
---
+++ lib/src/query_client_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:volt/src/query_client.dart';
+
+/// Provides a [QueryClient] to the widget tree.
+///
+/// This is used to provide a [QueryClient] to the widget tree. This is useful
+/// for providing a [QueryClient] to the [useQuery] and [useQueries] hooks.
+class QueryClientProvider extends InheritedWidget {
+  final QueryClient client;
+
+  const QueryClientProvider({
+    super.key,
+    required this.client,
+    required super.child,
+  });
+
+  static QueryClient of(BuildContext context) {
+    final provider = context.dependOnInheritedWidgetOfExactType<QueryClientProvider>();
+    if (provider == null) {
+      throw Exception('No QueryClientProvider found in context');
+    }
+    return provider.client;
+  }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    if (oldWidget is! QueryClientProvider) {
+      return true;
+    }
+
+    return oldWidget.client != client;
+  }
+}

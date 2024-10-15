9553d9fc4bcf183d4df5adc424d222dfc6d6d49b
Gil Sinclair-Julio <gil@golightyear.com>
2024-10-15 11:22:06 +0100

    Export useQueryClient hook

--- lib/volt.dart
+++ lib/volt.dart
@@ -3,6 +3,7 @@ library volt;
 export 'src/debug/volt_stats.dart';
 export 'src/hooks/use_queries.dart';
 export 'src/hooks/use_query.dart';
+export 'src/hooks/use_query_client.dart';
 export 'src/persister/persister.dart';
 export 'src/query.dart';
 export 'src/query_client.dart';
